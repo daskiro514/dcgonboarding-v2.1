757f62b6b97c20d6a565734f8b8966fb6ad248da
daskiro514 <luckybuzstar@gmail.com>
2021-09-09 23:55:08 +0900

    fixed

--- routes/api/partner.js
+++ routes/api/partner.js
@@ -453,11 +453,6 @@ router.get('/getPartnerTransactions/:id', async (req, res) => {
   })
 
   // FOR DEFAULT SUBSCRIPTION UPDATE
-  // let product1 = await Product.findOneAndRemove({ name: 'DCG Mastermind' })
-  // await stripe.products.del(product1.stripeProductID)
-  let product2 = await Product.findOneAndRemove({ stripeProductID: 'prod_K9MhgNag37xvj2' })
-  console.log(product2)
-  // await stripe.products.del(product2.stripeProductID)
   // let product1 = await Product.findOneAndUpdate({name: 'Master Mind Package'}, {
   //   description: ''
   // }, {new: true})
