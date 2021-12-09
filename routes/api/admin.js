0d154a2f2a64e53dbfa8a7d5309149f77e8d84a1
daskiro514 <luckybuzstar@gmail.com>
2021-12-10 04:39:55 +0900

    updated

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -581,13 +581,6 @@ router.get('/suspendProduct/:id', async (req, res) => {
 
 // CUSTOMERS
 router.get('/getAllCustomers', async (req, res) => {
-  const customers1 = await stripe.customers.list({
-    limit: 100,
-  })
-  // console.log(customers1)
-  customers1.data.forEach(customer => {
-    console.log(customer.name)
-  })
   const customersFromDB = await User.find({ type: 'customer' }).populate('purchasedProductID').populate('seller')
   const customers = customersFromDB.filter(customer => customer.customerStatus !== 'Deleted')
   res.json({
